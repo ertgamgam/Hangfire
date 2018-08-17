e5fe4dad88483784fcf91015cf37c2890a2758a2
ertugrul.gamgam <ertugrul.gamgam@bordatech.com>
2018-08-17 17:55:41 +0300

    Initial Commit

---
+++ CustomMW/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomMW.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private int a = 1;
+        // GET api/values
+        [Route("add")]
+        [HttpGet("{id}")]
+        public IEnumerable<string> AddJob(string id)
+        {
+            var jobId = BackgroundJob.Schedule(() => Console.WriteLine("background job"), TimeSpan.FromSeconds(10));
+            this.a++;
+
+            RecurringJob.AddOrUpdate(id, () => Print(id), Cron.Minutely, TimeZoneInfo.Local, "default");
+
+            return new string[] { "value1", "value2" };
+        }
+
+        [Route("delete")]
+        [HttpGet("{id}")]
+        public IEnumerable<string> DeleteJob(string id)
+        {
+            RecurringJob.RemoveIfExists(id);
+
+            return new string[] { "value1", "value2" };
+        }
+
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public void Print(string id)
+        {
+            Console.WriteLine($"Came from a method. My Id = {id} and this.a = {this.a}");
+        }
+    }
+}
---
+++ CustomMW/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.Storage;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CustomMW
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+           .AddJwtBearer(options =>
+           {
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateLifetime = true,
+                   ValidateIssuerSigningKey = true,
+
+                   ValidIssuer = "alesta.bordatech.com",
+                   ValidAudience = "alesta.bordatech.com",
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("fdsfsdfsdfsd"))
+               };
+           });
+
+            services.AddHangfire(m => m.UseSqlServerStorage(Configuration.GetConnectionString("DefaultConnection")));
+                        
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            app.Use(async (HttpContext ctx, Func<Task> next) =>
+            {
+                var request = ctx.Request;
+                //await ctx.Response.WriteAsync("1. asamada");
+                string test = ctx.Request.Headers["username"].ToString();
+
+                Console.WriteLine("REQUEST {0}", DateTime.Now.ToLongTimeString());
+                Console.WriteLine("{0}-{1}{2}", request.Method, request.Host, request.Path);
+
+                await next.Invoke();
+
+                var response = ctx.Response;
+                if (response.StatusCode == 401)
+                    await ctx.Response.WriteAsync("basarili");
+                Console.WriteLine("RESPONSE:{0}", DateTime.Now.ToLongTimeString());
+                Console.WriteLine("{0}\t({1}){2}", response.ContentType, response.StatusCode, (HttpStatusCode)response.StatusCode);
+            });
+
+            app.UseMvc();
+        }
+    }
+}
